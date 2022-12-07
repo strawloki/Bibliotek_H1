347b1a3a889080441a829f01e407fbac439f7969
strawloki <laurplayscevrea@gmail.com>
2022-12-07 11:27:08 +0100

    Finished menu

--- Bibliotek/Program.cs
+++ Bibliotek/Program.cs
@@ -12,8 +12,92 @@ namespace Bibliotek
             bibliotek.OpretLaaner(1, "Steven");
             bibliotek.OpretLaaner(2, "Stewart");
             bibliotek.OpretLaaner(3, "Ian");
-            Console.WriteLine(bibliotek.HentAlleLaanere());
-            
+            //Console.WriteLine(bibliotek.HentAlleLaanere());
+
+            while (true)
+            {
+                int valg = BibliotekPrompt();
+
+                switch (valg)
+                {
+                    case 1:
+                        bibliotek.HentBibliotek();
+                        break;
+
+                    case 2:
+                        OpretNyLaanerPrompt(bibliotek);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Laanere i biblioteket:");
+                        Console.WriteLine(bibliotek.HentAlleLaanere());
+                        break;
+
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+        }
+
+        static int BibliotekPrompt()
+        {
+            //Console.Clear();
+            int valg = -1;
+            bool choice = false;
+            while (!choice)
+            {
+
+                Console.Write("---------------------------------------------------\n");
+                Console.Write("Velkommen til bibliotekens program. Du kan vaelge folgende:\n");
+                Console.Write("1. Vis bibliotekens navn og dato\n");
+                Console.Write("2. Opret en ny laaner\n");
+                Console.Write("3. Udskriv laanere\n");
+                Console.Write("4. Afsult programmet\n");
+                Console.Write("---------------------------------------------------\n");
+                Console.Write("Indtast din valg: ");
+
+                string strValg = Console.ReadLine();
+
+
+                if (Int32.TryParse(strValg, out valg))
+                {
+                    if (valg < 1 || valg > 4)
+                        Console.WriteLine("Uglydig mulighed. Prov igen.");
+
+                    else
+                        choice = true;
+
+                }
+                else
+                    Console.WriteLine("Uglydig mulighed. Prov igen.");
+            }
+            return valg;
+        }
+
+        static void OpretNyLaanerPrompt(Bibliotek bib)
+        {
+            bool valueInputted = false;
+
+            while (!valueInputted)
+            {
+                Console.Write("Indast laaners navn: ");
+                string navn = Console.ReadLine();
+
+                Console.Write("\nIndast laaners nummer: ");
+                string numStr = Console.ReadLine();
+
+                int num = -1;
+                if (Int32.TryParse(numStr, out num))
+                {
+                    Console.WriteLine(bib.OpretLaaner(num, navn));
+                    valueInputted = true;
+
+                }
+                else Console.WriteLine("Ugyldig mulighed. Du skal indtaste kun en tal.");
+
+            }
         }
     }
 }
